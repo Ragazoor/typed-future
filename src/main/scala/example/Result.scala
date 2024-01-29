bc64261c69cb2e1f8b8cd821e1f484b67fe48ad2
Ragnar Englund <ragnar.englund@annotell.com>
2024-01-29 09:33:02 +0100

    Result.scala with flatmap

--- src/main/scala/example/Result.scala
+++ src/main/scala/example/Result.scala
@@ -1,25 +1,92 @@
 package example
 
-import scala.concurrent.Future
-import scala.concurrent.CanAwait
 import scala.concurrent.duration.Duration
-import scala.concurrent.ExecutionContext
-import scala.util.Either
-import scala.util.Try
+import scala.concurrent.{Await, ExecutionContext, Future}
 import scala.util.control.NonFatal
-import scala.concurrent.impl.Promise
-import scala.util.Success
+import scala.util.{Failure, Success, Try}
 
-trait Result[+E, +A] extends Future[A]
+class Result[+E <: Throwable, +A](future: Future[A]) {
+  def toFuture: Future[A] = future
 
-final case class Ok[+A](value: A) extends Result[Nothing, A]
+  // TODO: Fatal Error on exception in f
+  // Another with A => Try[B] to catch error
+  def map[B](f: A => B)(implicit ec: ExecutionContext): Result[E, B] =
+    new Result(future.map(f))
 
-final case class Panic[+E](value: E) extends Result[E, Nothing]
+  def flatMap[E2 >: E <: Throwable, B](f: A => Result[E2, B])(implicit ec: ExecutionContext): Result[E2, B] = {
+    val newFuture: Future[B] = future.flatMap { a =>
+      f(a).toFuture
+    }
+    new Result[E2, B](newFuture)
+  }
+
+  def onComplete(f: Try[A] => Unit)(implicit ec: ExecutionContext): Unit =
+    future.onComplete(f)
+
+}
 
 object Result {
 
-  val b = Future.successful(1)
 
-  final def apply[E<:Throwable,T](body: => T)(implicit ec: ExecutionContext): Result[E, T] =
-    Future.unit.map(_ => body)
+  final def fromFuture[A](body: Future[A])(implicit ec: ExecutionContext): Result[Throwable, A] =
+    new Result(body)
+
+  final def fromFuture[E <: Throwable, A](f: Throwable => E, body: Future[A])
+                                    (implicit ec: ExecutionContext): Result[E, A] =
+    new Result(body.recoverWith {
+      case e if NonFatal(e) => Future.failed(f(e))
+    })
+
+
+  final def successful[A](value: A): Result[Nothing, A] =
+    new Result(Future.successful(value))
+
+  final def sequence[E <: Throwable, A](results: Seq[Result[E, A]])(implicit ec: ExecutionContext): Result[E, Seq[A]] = {
+    val future = Future.sequence(results.map(_.toFuture))
+    new Result(future)
+  }
+
+  final def failed[E <: Throwable](exception: E): Result[E, Nothing] =
+    new Result(Future.failed(exception))
+
+  final def fromTry[A](body: Try[A]): Result[Throwable, A] =
+    new Result(Future.fromTry(body))
+
+  final def attempt[E <: Throwable, A](f: Throwable => E, body: A): Result[E, A] = {
+    val future = Future.fromTry {
+      Try(body)
+        .fold(
+          e => Failure(f(e)),
+          a => Success(a)
+        )
+    }
+    new Result(future)
+  }
+}
+
+object Main extends App {
+
+  import scala.concurrent.ExecutionContext.Implicits.global
+
+  sealed abstract class RootError extends Throwable
+  case class MyError(error: Throwable) extends RootError
+  case class MyError2(error: Throwable) extends RootError
+
+  private val result: Result[RootError, Unit] = for {
+    a <- Result.fromFuture(MyError, Future.successful(1)) // Result[MyError, Int]
+    b <- Result.fromFuture(MyError2, Future.successful(1)) // Result[MyError2, Int]
+    c <- Result.successful(1) // Result[Nothing, Int]
+  } yield {
+    println(a + b + c)
+  }
+
+  result.onComplete {
+    case Success(_) =>
+      System.exit(0)
+    case Failure(_) =>
+      System.exit(1)
+  }
+  Await.ready(result.toFuture, Duration.apply("10s"))
+
+
 }
