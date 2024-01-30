0b360194bd1add70a2495ea7e8873dda00337b44
Ragnar Englund <ragnar.englund@annotell.com>
2024-01-30 22:39:55 +0100

    Commit

--- src/test/scala/example/ResultSpec.scala
+++ src/test/scala/example/ResultSpec.scala
@@ -1,10 +1,13 @@
 package example
 
-import scala.concurrent.Future
 import munit.FunSuite
 
+import scala.concurrent.Future
+
 class HelloSpec extends FunSuite {
+
   import scala.concurrent.ExecutionContext.Implicits.global
+
   override def munitValueTransforms = super.munitValueTransforms ++ List(
     new ValueTransform(
       "Result",
@@ -13,6 +16,15 @@ class HelloSpec extends FunSuite {
       }
     )
   )
+
+  abstract class RootError(e: Throwable) extends Throwable(e)
+
+  case class MyError(e: Throwable) extends RootError(e)
+
+  case class MyError2(e: Throwable) extends RootError(e)
+
+  case class MyError3(e: Throwable) extends Throwable(e)
+
   test("Result using flatmap") {
     for {
       a <- Result.fromFuture(Future(1))
@@ -21,10 +33,6 @@ class HelloSpec extends FunSuite {
     } yield assertEquals(a + b + c, 6)
   }
   test("Result using flatmap with typed errors") {
-    abstract class RootError(e: Throwable) extends Throwable(e)
-    case class MyError(e: Throwable) extends RootError(e)
-    case class MyError2(e: Throwable) extends RootError(e)
-    case class MyError3(e: Throwable) extends Throwable(e)
     val a = for {
       a <- Result.fromFuture(MyError, Future(1))
       b <- Result.fromFuture(MyError2, Future(2))
@@ -32,15 +40,16 @@ class HelloSpec extends FunSuite {
     } yield assertEquals(a + b + c, 6)
     a.recover {
       case _: RuntimeException => assert(false)
-      case _: MyError2         => assert(false)
-      case _: MyError3         => assert(true)
+      case _: MyError2 => assert(false)
+      case _: MyError3 => assert(true)
     }
   }
   test("Result using sequence") {
     def getResult(i: Int) = Result.fromFuture(Future(i))
+
     Result
       .sequence(Seq(1, 2, 3).map(getResult))
-      .map(a => a.reduce(_ + _))
+      .map(_.sum)
       .map(sum => assertEquals(sum, 6))
   }
 
@@ -53,6 +62,15 @@ class HelloSpec extends FunSuite {
 
     Result
       .sequence(Seq(1, 2, 3).map(getResult))
+      .recoverWith { case _: IllegalArgumentException =>
+        Result.successful(assert(true))
+      }
+  }
+
+  test("Result using flatten") {
+    val result1 = Result.fromFuture(Future(1))
+    val result2 = Result.fromFuture(Future(result1))
+      result2.flatten
       .recoverWith { case _: IllegalArgumentException =>
         Result.successful(assert(true))
       }
