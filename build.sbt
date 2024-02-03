4247e472c84acf6687e7ede3da1a48cae8f97ae7
Ragnar Englund <ragnar.englund@annotell.com>
2024-02-03 11:38:18 +0100

    subfolders

--- build.sbt
+++ build.sbt
@@ -1,17 +1,28 @@
 import Dependencies._
 import BuildHelper._
 
-ThisBuild / scalaVersion     := "2.13.12"
-ThisBuild / version          := "0.1.0-SNAPSHOT"
-ThisBuild / organization     := "com.example"
+ThisBuild / version := "0.1.0-SNAPSHOT"
+ThisBuild / organization := "com.example"
 ThisBuild / organizationName := "ragz"
 
-lazy val root = (project in file("."))
+lazy val root =
+  project
+    .in(file("."))
+    .settings(publish / skip := true)
+    .aggregate(result)
+
+
+lazy val result = module("result", "result")
   .enablePlugins(BuildInfoPlugin)
+  .settings(buildInfoSettings("com.example"))
   .settings(
-    name := "scala-result",
     libraryDependencies += munit % Test
   )
-  .settings(buildInfoSettings("com.example"))
+  .settings(stdSettings("result"))
+
+
+def module(moduleName: String, fileName: String): Project =
+  Project(moduleName, file(fileName))
+    .settings(stdSettings(moduleName))
 
 // See https://www.scala-sbt.org/1.x/docs/Using-Sonatype.html for instructions on how to publish to Sonatype.
--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -1,13 +1,92 @@
 import sbt.Keys._
+import sbt.{CrossVersion, ThisBuild}
+import sbtbuildinfo.BuildInfoKeys._
 import sbtbuildinfo._
-import BuildInfoKeys._
 
 
 object BuildHelper {
+  private val Scala212 = "2.12.18"
+  private val Scala213 = "2.13.12"
+  private val Scala3 = "3.3.1"
+
+  private val stdOptions     = Seq(
+    "-encoding",
+    "UTF-8",
+    "-feature",
+    "-unchecked",
+    "-deprecation",
+    "-Xfatal-warnings",
+    "-language:higherKinds",
+    "-language:existentials"
+  )
+  private val stdOpts213And3 = Seq(
+    "-Wunused:privates",
+    "-Wunused:imports",
+    "-Wunused:params",
+    "-Wvalue-discard"
+  )
+
+  private val stdOpts3 = Seq(
+    "-explain-types"
+  )
+
+  private val stdOpts213 = Seq(
+    "-Ywarn-numeric-widen",
+    "-Ywarn-value-discard",
+    "-Xlint:_,-type-parameter-shadow",
+    "-explaintypes",
+    "-Yrangepos",
+    "-Wunused:patvars",
+    "-Xlint:-infer-any"
+  )
+
+  private val stdOptsUpto212 = Seq(
+    "-Ywarn-numeric-widen",
+    "-Ywarn-value-discard",
+    "-Xlint:_,-type-parameter-shadow",
+    "-explaintypes",
+    "-Yrangepos",
+    "-Xfuture",
+    "-Ypartial-unification",
+    "-Ywarn-nullary-override",
+    "-Yno-adapted-args",
+    "-Ywarn-inaccessible",
+    "-Ywarn-nullary-unit",
+    "-Ywarn-unused-import",
+    "-Xlint:-infer-any"
+  )
+
+  private def extraOptions(scalaVersion: String) =
+    CrossVersion.partialVersion(scalaVersion) match {
+      case Some((3, _)) => stdOpts3 ++ stdOpts213And3
+      case Some((2, 13)) => stdOpts213 ++ stdOpts213And3
+      case Some((2, 12)) =>
+        Seq(
+          "-opt-warnings",
+          "-Ywarn-extra-implicit",
+          "-Ywarn-unused:_,imports",
+          "-Ywarn-unused:imports",
+          "-opt:l:inline",
+          "-opt-inline-from:<source>"
+        ) ++ stdOptsUpto212
+      case _ =>
+        Seq("-Xexperimental") ++ stdOptsUpto212
+    }
+
+
   def buildInfoSettings(packageName: String) =
     Seq(
       buildInfoKeys    := Seq[BuildInfoKey](name, version, scalaVersion, sbtVersion, isSnapshot),
       buildInfoPackage := packageName,
       buildInfoObject  := "BuildInfo"
+    )
+
+  def stdSettings(prjName: String) =
+    Seq(
+      name := s"$prjName",
+      crossScalaVersions := Seq(Scala212, Scala213, Scala3),
+      ThisBuild / scalaVersion := Scala213,
+      scalacOptions := stdOptions ++ extraOptions(scalaVersion.value),
+      incOptions ~= (_.withLogRecompileOnMacro(false))
     )
 }
