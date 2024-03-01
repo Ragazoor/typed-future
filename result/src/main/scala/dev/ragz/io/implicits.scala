e880be672d9fa00e437b243269a8ae0cf4bc77b8
Ragnar Englund <ragnar.englund@annotell.com>
2024-03-01 13:47:10 +0100

    fix tests

--- result/src/main/scala/dev/ragz/io/implicits.scala
+++ result/src/main/scala/dev/ragz/io/implicits.scala
@@ -14,7 +14,7 @@ object implicits {
   implicit def ioToStdFutureF0[E <: Throwable, A](f0: () => IO[E, A]): () => StdFuture[A] =
     () => f0().toFuture
 
-  implicit def ioToStdFutureF1[E <: Throwable, X1, B](f1: A => IO[E, B]): X1 => StdFuture[B] =
+  implicit def ioToStdFutureF1[E <: Throwable, X1, B](f1: X1 => IO[E, B]): X1 => StdFuture[B] =
     x1 => f1(x1).toFuture
 
   implicit def ioToStdFutureF2[E <: Throwable, X1, X2, C](f2: (X1, X2) => IO[E, C]): (X1, X2) => StdFuture[C] =
