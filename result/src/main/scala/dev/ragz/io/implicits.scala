78c113e2eafda889e71a215871e3179cee934c0a
Ragnar Englund <ragnar.englund@annotell.com>
2024-03-01 12:53:34 +0100

    Added migration implicits

--- result/src/main/scala/dev/ragz/io/implicits.scala
+++ result/src/main/scala/dev/ragz/io/implicits.scala
@@ -4,6 +4,16 @@ import scala.concurrent.{ Future => StdFuture }
 import scala.language.implicitConversions
 
 object implicits {
-  implicit def stdFutureToIO[A](future: StdFuture[A]): IO[Throwable, A] =
-    IO.fromFuture(future)
+  implicit class StdFutureToIo[A](val future: StdFuture[A]) {
+    def io: IO[Throwable, A] = IO.fromFuture(future)
+  }
+
+  implicit def ioToStdFuture[E <: Throwable, A](io: IO[E, A]): StdFuture[A] =
+    io.toFuture
+
+  implicit def ioToStdFutureF0[E <: Throwable, A](f0: () => IO[E, A]): () => StdFuture[A] =
+    () => f0().toFuture
+
+  implicit def ioToStdFutureF1[E <: Throwable, A, B](f1: A => IO[E, B]): A => StdFuture[B] =
+    x1 => f1(x1).toFuture
 }
