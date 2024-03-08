cbb7e3ed1116576938c1efb46d704a427dbec437
Ragnar Englund <ragnar.englund@annotell.com>
2024-03-08 11:44:37 +0100

    Add type aliases

--- result/src/main/scala/dev/ragz/io/package.scala
+++ result/src/main/scala/dev/ragz/io/package.scala
@@ -3,4 +3,15 @@ package dev.ragz
 package object io {
   type Future[+A] = IO[Throwable, A]
   type ExecutionContext = scala.concurrent.ExecutionContext
+
+  /**
+   * Type alias for [[scala.concurrent.ExecutionContextExecutor ExecutionContextExecutor]].
+   */
+  type ExecutionContextExecutor = scala.concurrent.ExecutionContextExecutor
+
+  /**
+   * Type alias for [[scala.concurrent.ExecutionContextExecutorService ExecutionContextExecutorService]].
+   */
+  type ExecutionContextExecutorService = scala.concurrent.ExecutionContextExecutorService
+
 }
