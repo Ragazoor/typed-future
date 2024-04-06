b314a4cb20ab20f6e83f79b0a58190fc4fc3cad8
Ragnar Englund <ragnar.englund@annotell.com>
2024-04-06 14:23:48 +0200

    Give up on supporting everything on scala.concurrent

--- result/src/main/scala/io/github/ragazoor/package.scala
+++ result/src/main/scala/io/github/ragazoor/package.scala
@@ -1,19 +1,5 @@
 package io.github
 
 package object ragazoor {
-  type Future[+A]       = IO[Throwable, A]
-  type ExecutionContext = scala.concurrent.ExecutionContext
-  type Awaitable[+A]    = scala.concurrent.Awaitable[A]
-  type CanAwait         = scala.concurrent.CanAwait
-
-  /**
-   * Type alias for scala.concurrent.ExecutionContextExecutor.
-   */
-  type ExecutionContextExecutor = scala.concurrent.ExecutionContextExecutor
-
-  /**
-   * Type alias for scala.concurrent.ExecutionContextExecutorService.
-   */
-  type ExecutionContextExecutorService = scala.concurrent.ExecutionContextExecutorService
-
+  type Future[+A] = IO[Throwable, A]
 }
