5d8023f55ea99db306f740197c9ab2e929683b0b
Ragnar Englund <ragnar.englund@annotell.com>
2024-04-14 12:27:39 +0200

    update example

--- examples/src/main/scala/examples/UserServiceFutureExample.scala
+++ examples/src/main/scala/examples/UserServiceFutureExample.scala
@@ -1,11 +1,11 @@
 package examples
 
 import common.User
+import io.github.ragazoor.Future
 import io.github.ragazoor.implicits.StdFutureToTask
 import io.github.ragazoor.migration.implicits._
-import io.github.ragazoor.{ Future, Task }
 
-import scala.concurrent.{ ExecutionContext, Future => StdFuture }
+import scala.concurrent.{ Future => StdFuture }
 
 /*
  * Imagine this is in a third party library
@@ -14,19 +14,15 @@ trait UserProcess {
   def process(id: StdFuture[User]): StdFuture[User]
 }
 
-class UserServiceFutureExample(userProcess: UserProcess)(implicit ec: ExecutionContext) {
+class UserServiceFutureExample(userProcess: UserProcess) {
 
   /* implicit conversion in io.github.ragazoor.migration.implicits._ converts
    * the Task to a Future
    */
-  def getUser(id: Int): Future[User] =
-    userProcess.process {
-      Task.successful(User("Test name", 44))
-    }.toTask
+  def proccessUser(user: Future[User]): Future[User] =
+    userProcess.process(user).toTask
 
   // Does the same thing without implicits, but more migration needed
-  def getUserExplicit(id: Int): Future[User] =
-    userProcess.process {
-      Task.successful(User("Test name", 44)).toFuture // Here the conversion to future is explicit
-    }.toTask
+  def getUserExplicit(user: Future[User]): Future[User] =
+    userProcess.process(user.toFuture).toTask
 }
