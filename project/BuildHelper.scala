ee757cfeccc2162fb9883e23cf5a919d7b17cd47
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2024-10-19 18:30:18 +0200

    Update scala-library to 2.13.15 (#20)

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -5,7 +5,7 @@ import sbtbuildinfo._
 
 object BuildHelper {
   private val Scala212 = "2.12.18"
-  private val Scala213 = "2.13.12"
+  private val Scala213 = "2.13.15"
   private val Scala3   = "3.3.4"
 
   private val stdOptions     = Seq(
